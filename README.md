87fe07e4ac4d453445ffff56119a340c2665200e
Clement <clementlecuillier@hotmail.fr>
2018-02-20 20:19:52 +0100

    final push

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 * Page d'index : clique sur les noms des sites pour accéder à la LP correspondante. **Attention !!*** Il y a un beug, il faut reloader la page pour l'image de background se load....
 
 * Checkr : Je n'ai pas insérer de liens, si ce n'est la vidéo qui ne s'affiche pas !
+
+* Flynn : Pareil, pas sûr que la video fonctionne !
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module LandingPagesClem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.1#cette ligne et celle du dessous permettent l'ajout de la vidéo
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
+    config.assets.enabled = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
